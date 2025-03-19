6109e838492aba241823a07f0ab5d7ad2a90e844
Dmitry <45402773+dibobik@users.noreply.github.com>
2025-03-19 16:54:36 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBl-cYM8XtkB4Yceg7KwvsmIWBZfBsCXSo",
-    authDomain: "generatorideaapp.firebaseapp.com",
-    projectId: "generatorideaapp",
-    storageBucket: "generatorideaapp.firebasestorage.app",
-    messagingSenderId: "125014306376",
-    appId: "1:125014306376:web:1a21903396d6ae7ca7727d",
-    measurementId: "G-4CRQTRYCV0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase App
